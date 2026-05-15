59683f452f6f3dcc0234fe18bc28ef9d94ba5078
DanBe202 <opaetdaniel2004@gmail.com>
2026-05-15 11:20:23 +0300

    Initial

---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ApplicationRecord
+  enum :priority, { low: 0, medium: 1, high: 2 }
+  validates :title, presence: true
+end
---
+++ db/migrate/20260515071603_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :category
+      t.integer :priority, default: 0
+      t.date :due_date
+      t.integer :estimated_hours
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
